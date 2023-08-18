a269ea7f6e3f6b0da1c3a839f5f267f80e4b9cec
an2084 <angelanawang47@gmail.com>
2023-08-18 06:53:13 -0400

    Embedded RouteStack to drawer instead of RouteScreen so that all screens work

--- navigation/drawer.js
+++ navigation/drawer.js
@@ -1,12 +1,12 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import RouteScreen from '../screens/RouteScreen';
+import { RouteStack } from './stack';
 
 const Drawer = createDrawerNavigator();
 
 export const MyDrawer = () => { 
     return (
         <Drawer.Navigator>
-            <Drawer.Screen name="RouteScreen" component={RouteScreen} />
+            <Drawer.Screen name="RouteStack" component={RouteStack} />
             {/* <Drawer.Screen name="Article" component={Article} /> */}
         </Drawer.Navigator>
     );
