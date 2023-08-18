e4e1cc4e8dc72a976967e34e4518ffd8027f2cc7
an2084 <angelanawang47@gmail.com>
2023-08-18 06:31:16 -0400

    Added padding and refresh to RouteList

--- components/RouteList.js
+++ components/RouteList.js
@@ -1,16 +1,26 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, RefreshControl } from 'react-native';
 import { dummy_data } from '../data/dummy';
+import RouteItem from './RouteItem';
 
 const RouteList = () => {
     const renderItem = ({ item }) => {
-        return <Text>{item.title}</Text>
+        return <RouteItem id={item.id} title={item.title} date_added={item.date_added} distance={item.distance} duration={item.duration} />
     }
+    const handleRefresh = () => {
+        console.log('Refreshing..');
+    };
     return (
         <View>
             <FlatList
                 data={dummy_data}
                 keyExtractor={item => item.id}
                 renderItem={renderItem}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={false}
+                        onRefresh={() => handleRefresh}
+                    />
+                }
             />
         </View>
     );
