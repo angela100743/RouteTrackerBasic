29b3d6318ef507d9d40b48f91f259e4c6cb3a617
an2084 <angelanawang47@gmail.com>
2023-08-17 22:41:10 -0400

    Created basic IndvRouteScreen

---
+++ screens/IndvRouteScreen.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndvRouteScreen = () => {
+    return (
+        <View style={styles.screen}>
+            <Text>IndvRoute Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        padding: 20,
+    }
+});
+
+export default IndvRouteScreen;
