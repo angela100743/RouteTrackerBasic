9a868ba1505bfd5f21616643084014d9e54c01b2
an2084 <angelanawang47@gmail.com>
2023-08-18 06:41:39 -0400

    Added clickable option on RouteList to expand into IndvRoute with all details showing

---
+++ components/RouteItem.js
@@ -0,0 +1,27 @@
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const RouteItem = ({id, title, date_added, distance, duration}) => {
+    const navigation = useNavigation()
+    
+    return (  
+        <TouchableOpacity style={styles.card} onPress={()=>navigation.navigate("IndvRoute",{routeId:id, title, date_added, duration, distance})}>
+            <Text>{title}</Text>
+            <Text>{date_added}</Text>
+            <Text>{distance}</Text>
+            <Text>{duration}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        borderWidth: 1,
+        borderColor: '#c5c5c5',
+        borderRadius: 10,
+        marginVertical: 5,
+        padding: 30,
+    }
+})
+
+export default RouteItem;
--- screens/IndvRouteScreen.js
+++ screens/IndvRouteScreen.js
@@ -1,10 +1,18 @@
+import { useRoute } from '@react-navigation/native';
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const IndvRouteScreen = () => {
+    const route = useRoute()
+    // allows us to pass in the rest of parameteres to the IndvRouteScreen
+    const { routeId, title, date_added, distance, duration } = route.params
+
     return (
         <View style={styles.screen}>
-            <Text>IndvRoute Screen</Text>
+            <Text>{title}</Text>
+            <Text>{date_added}</Text>
+            <Text>{distance}</Text>
+            <Text>{duration}</Text>
         </View>
     );
 };
