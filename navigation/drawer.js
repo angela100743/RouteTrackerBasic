4a810837b007016e4857e26bd6a4579c4ef4bb85
an2084 <angelanawang47@gmail.com>
2023-08-18 10:00:37 -0400

    Added logo and more info

--- navigation/drawer.js
+++ navigation/drawer.js
@@ -1,16 +1,41 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { RouteStack } from './stack';
-
+import { ProfileStack, RouteStack } from './stack';
+import { DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import { SafeAreaView, View, Image, Linking } from 'react-native';
 const Drawer = createDrawerNavigator();
 
 export const MyDrawer = () => { 
     return (
-        <Drawer.Navigator screenOptions={{headerShown: false}}>
-            <Drawer.Screen name="RouteStack" 
-                component={RouteStack} />  
-                {/* options={{title:"My Routes"}}  */}
-            
-            {/* <Drawer.Screen name="Article" component={Article} /> */}
+        <Drawer.Navigator
+            drawerContent={(props) => {
+                return (
+                    <SafeAreaView style={{ flex: 1, paddingTop: 20, backgroundColor: '#AFE1AF'}}>
+                        <View style={{justifyContent:'center', alignItems:'center', height: 140}}>
+                            <Image
+                                style={{ width: 100, height: 100, resizeMode:'contain' }}
+                                source={require("../assets/ktmtransitlogo-circle.png")}
+                            
+                            />
+                        </View>
+                        <DrawerItemList {...props} />
+                        <DrawerItem 
+                            label="More info"
+                            onPress={()=>Linking.openURL('https://kathmandupost.com/national/2018/04/06/bus-syndicate-retreats-after-tough-govt-action')}
+                        />
+                    </SafeAreaView>
+                );
+            }}
+                screenOptions={{
+                    headerShown: false,
+                }}
+        >
+            <Drawer.Screen name="Saved Routes" 
+                component={RouteStack}  
+                options={{title:"My Routes"}}  />
+            <Drawer.Screen name="ProfileStack" 
+                component={ProfileStack} 
+                options={{title: "Profile"}}/>  
+
         </Drawer.Navigator>
     );
 }
