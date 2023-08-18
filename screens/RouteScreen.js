673920a28c2a593f30c8d0dd7bf9c8322eb1b484
an2084 <angelanawang47@gmail.com>
2023-08-17 22:46:05 -0400

    Added navigation to IndvRouteScreen

--- screens/RouteScreen.js
+++ screens/RouteScreen.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const RouteScreen = () => {
+    const navigation = useNavigation()
     return (
         <View style={styles.screen}>
             <Text>Route Screen</Text>
+            <Button title="Go to IndvRoute" onPress={() => navigation.navigate('IndvRoute')} />
         </View>
     );
 };
